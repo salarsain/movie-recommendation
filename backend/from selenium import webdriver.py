41c0ea59e264543706c96c2251259e570d61f3f2
Salar Ahmed <salarsain2456@gmail.com>
2025-12-27 15:33:28 +0500

    Delete backend/from selenium import webdriver.py

--- backend/from selenium import webdriver.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
-driver.get("https://www.google.com")
-print(driver.title)
-driver.quit()
